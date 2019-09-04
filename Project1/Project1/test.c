17c78816d4ed0013fa235d2f78f1d167e85492bc
cactus-kb <2140934374@qq.com>
2019-09-04 22:09:37 +0800

    代码练习

---
+++ Project1/Project1/test.c
@@ -0,0 +1,24 @@
+//给定两个整形变量的值，将两个值的内容进行交换。
+#include<windows.h>
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	scanf("%d%d", &a,&b);
+	printf("a=,b= ");
+	a = c;
+	c = b;
+	b = c;
+	printf("a= ,b= ");
+	system("pause");
+	return 0;
+}
+
+/*int main()
+{
+	printf("haha");
+	system("pause");
+	return 0;
+}*/
