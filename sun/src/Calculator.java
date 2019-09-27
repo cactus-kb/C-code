9e1593096abf5d5adcb88673a2c24846d65482ac
cactus-kb <2140934374@qq.com>
2019-09-27 10:13:02 +0800

    练习

---
+++ sun/src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+    public int num1 = 1;
+    private double num2;
+
+//    public void add(double a,double b){
+//        System.out.println("ret = " + (a + b));
+//    }
+//
+//    public void sub(double a,double b){
+//        System.out.println( a - b);
+//    }
+//
+//    public void mul(double a,double b){
+//        System.out.println( a * b);
+//    }
+//
+//    public void div(double a,double b){
+//        System.out.println( a / b);
+//    }
+    public void sun(){
+        System.out.println("num1 = " + num1);
+    }
+}
---
+++ sun/src/Cl.java
@@ -0,0 +1,17 @@
+public class Cl {
+    public static void mian(String[] args){
+//        Calculator c1 = new Calculator();
+//        c1.add(1,3);
+//
+//        Calculator c2 = new Calculator();
+//        c1.add(1,3);
+//
+//        Calculator c3 = new Calculator();
+//        c1.add(5,3);
+//
+//        Calculator c4 = new Calculator();
+//        c1.add(1,3);
+        Calculator a = new Calculator();
+        a.sun();
+    }
+}
