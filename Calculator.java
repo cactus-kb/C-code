20dd24b68ef3d9b6bb587dacb0beacdc190f70e7
cactus-kb <2140934374@qq.com>
2019-10-28 09:07:14 +0800

    练习

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    private int num1;
+    private int num2;
+
+
+    public Calculator() {
+
+    }
+
+    public double Add(double a,double b) {
+        return a + b;
+    }
+
+    public double Sub(double a,double b) {
+        return a - b;
+    }
+
+    public double Mul(double a, double b) {
+        return a * b;
+    }
+
+    public double Div(double a, double b) {
+        return a / b;
+    }
+}
