8883290f0ad0ff960fd817386b9f1e951952126e
cactus-kb <2140934374@qq.com>
2020-01-11 22:11:25 +0800

    practice

---
+++ Servlet/src/main/java/com/LoginServlet.java
@@ -0,0 +1,19 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Severlet haha</h1>");
+    }
+}
+
